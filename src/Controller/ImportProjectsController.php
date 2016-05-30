5ad9a88755b9cd923f368d7ee95bfebc03f969b4
christophb <chrib@hotmail.de>
2016-05-30 13:09:32 +0000

    finished first iteration of vocabularies; some refactoring

--- src/Controller/ImportProjectsController.php
+++ src/Controller/ImportProjectsController.php
@@ -2,30 +2,43 @@
 
 namespace Drupal\import_projects\Controller;
 
+use Exception;
+
 use Drupal\taxonomy\Entity\Vocabulary;
 use Drupal\taxonomy\Entity\Term;
 
 class ImportProjectsController {
+    const VOCABULARY = 'Importiert mit Modul';
+    const VID = '0815';
+    
     public function import() {
+        try {
+            self::createTaxonomy(
+                self::VID,
+                self::VOCABULARY, 
+                [ 'test 1', 'test 2', 'test 3', 'test 4' ]
+            );
         
-        $this->import_taxonomy(
-            'mit Service generiert', 
-            [ 'test 1', 'test 2', 'test 3', 'test 4' ]
-        );
+            self::createProject();
 
-        return [ '#title'  => 'Success!' ];
+            return [ '#title'  => 'Success!' ];
+        } catch (Exception $e) {
+            return [ 
+                '#title' => 'Error!', 
+                '#markup' => $e->getMessage()
+            ];
+        }
     }
     
-    private function import_taxonomy($name, $categories) {
-        if (!$name or !$categories) return;
+    private function createTaxonomy($vid, $name, $categories) {
+        // TODO hierarchies are ignored
+        if (!$vid) throw new Exception('Error: parameter $vid missing');
+        if (!$name) throw new Exception('Error: parameter $name missing');
+        if (!$categories) throw new Exception('Error: parameter $categories missing');
+    
+        $vocabulary = self::createVocabulary($vid, $name);
         
-        $vocabulary = Vocabulary::create([
-            'name' => 'mit Service generiert',
-            'weight' => 0,
-            'vid' => '0815'
-        ]);
-        $vocabulary->save();
-        
+        // TODO: get vid of created vocabulary
         $categories_vocabulary = '0815';
         
         foreach ($categories as $category) {
@@ -36,4 +49,20 @@ class ImportProjectsController {
             ])->save();
         }
     }
+    
+    private function createVocabulary($vid, $name) {
+        if (!$vid) throw new Exception('Error: parameter $vid missing');
+        if (!$name) throw new Exception('Error: parameter $name missing');
+        
+        $vocabulary = Vocabulary::create([
+            'name' => $name,
+            'weight' => 0,
+            'vid' => $vid
+        ]);
+        $vocabulary->save();
+        
+        return $vocabulary;
+    }
+    
+    private function createProject() {}
 }
